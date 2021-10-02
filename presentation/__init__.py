7163db219df2205b13e637275fce364df6d92585
Kyle Southwick <kyeguy00@hotmail.com>
2021-10-01 18:43:11 -0600

    I added my greeting

--- presentation/__init__.py
+++ presentation/__init__.py
@@ -1,7 +1,9 @@
 #main file 
 from ortiz import print_greeting
+from southwick import southwick_greeting
 
 
 print("\nWelcome to our collaborative program.\n")
 
 print_greeting()
+southwick_greeting()
