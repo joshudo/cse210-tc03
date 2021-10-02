5ffc434ee4487108cce7352427ee3dd2f73b9fab
Kyle Southwick <kyeguy00@hotmail.com>
2021-10-02 17:54:17 -0600

    I added my bio and my greeting

--- presentation/__init__.py
+++ presentation/__init__.py
@@ -2,10 +2,11 @@
 from ortiz import print_greeting
 from Seaver import print_greeting_seaver
 from cruz import isaac_greating
-
+from southwick import southwick_greeting
 
 print("\nWelcome to our collaborative program.\n")
 
 print_greeting()
 print_greeting_seaver()
 isaac_greating()
+southwick_greeting()
---
+++ presentation/southwick.py
@@ -0,0 +1,2 @@
+def southwick_greeting():
+    print("Hey! I'm Kyle Southwick")
